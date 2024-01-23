c36964cbbdc2988e33f6f6717a3d9aff701c0646
BekiHabesha <bereketdere@gmail.com>
2024-01-23 12:30:02 -0800

    106-bitonic_sort.c - N0 10-Bitonic sort.

---
+++ 106-bitonic_sort.c
@@ -0,0 +1,148 @@
+/*
+*
+*  📝🔡File_name🔠📒: 106-bitonic_sort.c
+*    📑🔢Task_no🔢📜: 10
+* 📖🔤Task_Title🔤📃: Bitonic sort
+*
+*  📔📚Functions📓🗳: void bitonic_sort, void bitonic_seq,
+*                       void bitonic_merge, void swap_ints.
+* 🗓⌚️Created On🕰📅: january 22, 2024
+*
+*     🧧🔠Project Title🔤🧰: 0x1B. C - Sorting algorithms & Big O
+* 🗃📀GitHub repository📀💽: sorting_algorithms
+*  💾🗂GitHub Directory📁🗃: sorting_algorithms
+*
+*  💻🖍️Author🖊️🪪: Bereket Dereje Mekkonen
+*  🌍🔢Cohort🔢🌍: 19
+* 🪙📱Program📲🎖: Full Stack
+*
+*/
+
+#include "sort.h"
+
+void swap_ints(int *a, int *b);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow);
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_sort(int *array, size_t size);
+
+/**
+ * swap_ints - It Swaps two integers in an array.
+ *
+ * Author: Bereket Dereje Mekkonen.
+ *
+ * @a: The first integer to swap.
+ *
+ * @b: The second integer to swap.
+ */
+
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+
+/**
+ * bitonic_merge - It Sorts a bitonic sequence
+ *                 inside an array of integers.
+ *
+ * Author: Bereket Dereje Mekkonen.
+ *
+ * @array: An array of integers.
+ *
+ * @size: The size of the array.
+ *
+ * @start: The starting index of the sequence
+ *         in array to sort.
+ *
+ * @seq: The size of the sequence to sort.
+ *
+ * @flow: The direction to sort in.
+ */
+
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow)
+{
+	size_t i, jump = seq / 2;
+
+	if (seq > 1)
+	{
+		for (i = start; i < start + jump; i++)
+		{
+			if ((flow == UP && array[i] > array[i + jump]) ||
+			    (flow == DOWN && array[i] < array[i + jump]))
+				swap_ints(array + i, array + i + jump);
+		}
+		bitonic_merge(array, size, start, jump, flow);
+		bitonic_merge(array, size, start + jump, jump, flow);
+	}
+}
+
+
+/**
+ * bitonic_seq - It Converts an array of integers
+ *               into a bitonic sequence.
+ *
+ * Author: Bereket Dereje Mekkonen.
+ *
+ * @array: An array of integers.
+ *
+ * @size: The size of the array.
+ *
+ * @start: The starting index of a block of
+ *         the building bitonic sequence.
+ *
+ * @seq: The size of a block of the building
+ *       bitonic sequence.
+ *
+ * @flow: The direction to sort the bitonic
+ *        sequence block in.
+ */
+
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
+{
+	size_t cut = seq / 2;
+	char *str = (flow == UP) ? "UP" : "DOWN";
+
+	if (seq > 1)
+	{
+		printf("Merging [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+
+		bitonic_seq(array, size, start, cut, UP);
+		bitonic_seq(array, size, start + cut, cut, DOWN);
+		bitonic_merge(array, size, start, seq, flow);
+
+		printf("Result [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+	}
+}
+
+
+/**
+ * bitonic_sort - Sort an array of integers in ascending
+ *                order using the bitonic sort algorithm.
+ *
+ * Author: Bereket Dereje Mekkonen.
+ *
+ * @array: An array of integers.
+ *
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ *              Only it works for size = 2^k
+ *              where k >= 0
+ *              (ie. size equal to powers of 2).
+ */
+
+void bitonic_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	bitonic_seq(array, size, 0, size, UP);
+}
